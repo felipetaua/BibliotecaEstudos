e2592386e3ed16e3c5d2e9de88a26a2ac938135a
felipetaua <taua.felipee@gmail.com>
2025-05-16 21:35:59 -0300

    aprendendo sobre o tipo booleano

--- JavaScript/3 - tipos-de-dados.js
+++ JavaScript/3 - tipos-de-dados.js
@@ -66,3 +66,25 @@ const person = {
 console.log(person.address.number)
 console.log(person.address.number.what) // nesse caso não existe what então vai dar undefined
 
+
+
+/* BOOLEAN */
+
+let myNumberOne = 10
+let myNumberTwo = 20
+
+let myNumberThird = 20
+let myNumberFour = 30
+
+// Atribuição então agora o myNumberOne é 20 esta atribuindo o valor ao myNumberOne
+myNumberOne = myNumberTwo
+
+console.log(myNumberOne)
+
+// == -> sinal de comparação então esse valor vai ser true por conta da atribuição, para ser falso eu precisaria remover a atribuição myNumberOne = myNumberTwo
+console.log(myNumberOne == myNumberThird)
+
+
+// resultado falso porque eles não são iguais
+console.log(myNumberThird == myNumberFour)
+
