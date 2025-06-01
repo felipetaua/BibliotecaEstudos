9364e090ebd41aeffa551745aaf8c50536fb1435
felipetaua <taua.felipee@gmail.com>
2025-05-31 22:50:28 -0300

    adicionando os numeros aleatórios

---
+++ JavaScript/Exercicios-Expecifícos/Math/3 - ex2.js
@@ -0,0 +1,6 @@
+// CALCULADORA DE POTÊNCIA
+//     - Solicite dois números:
+//       → Primeiro: Base
+//       → Segundo: Expoente
+//     - Calcule e mostre o resultado usando Math.pow() ou **.
+
