f8a5f2f2dd9550f4cda9e47669b101bcdf225b6e
felipetaua <taua.felipee@gmail.com>
2025-05-31 22:54:46 -0300

    adicionando o terceiro exercícios js

--- JavaScript/Exercicios-Expecifícos/Math/3 - ex2.js
+++ JavaScript/Exercicios-Expecifícos/Math/3 - ex2.js
@@ -4,3 +4,11 @@
 //       → Segundo: Expoente
 //     - Calcule e mostre o resultado usando Math.pow() ou **.
 
+const base =  25
+const expoente = 5
+
+const calculoPotencia = base ** expoente
+
+const calcPot = Math.pow(base, expoente)// ex2
+
+console.log("O resultado é:", calculoPotencia)
