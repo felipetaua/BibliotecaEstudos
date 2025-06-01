9c61bdc0cbc69016d6211109e13c0b325e2a6eeb
felipetaua <taua.felipee@gmail.com>
2025-06-01 14:05:31 -0300

    adicionando o exercicio de acertar o numero sorteado

---
+++ JavaScript/Exercicios-Expecifícos/Math/12 - ex11.js
@@ -0,0 +1,4 @@
+// 🧠 SISTEMA DE LOTERIA SIMPLES
+//     - O usuário escolhe 6 números entre 1 e 60.
+//     - O sistema sorteia 6 números aleatórios (sem repetir).
+//     - Mostre quantos números ele acertou.
