204a37bfe53ba7a2140efba2ef58c0af2338ba33
felipetaua <taua.felipee@gmail.com>
2025-06-04 10:09:13 -0300

    aprendendo sobre métodos array

---
+++ JavaScript/30 - array.js
@@ -0,0 +1,22 @@
+/*
+    ARRAYS - MÉTODOS
+*/
+
+const students = ['João', 'Maria', 'Pedro', 'Carla', 'Bia', 'Murilo']
+
+
+students.push('Rodolfo')
+students.push('Tauã')
+
+console.log(students)
+
+// push
+// length
+// sort
+// delete
+
+
+// splice (unir / ligar)
+//slice (fatiar)
+// pop
+// shift
