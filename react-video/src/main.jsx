329cdf61afa2eb257b6a3409ba4a9ad820861ef1
felipetaua <taua.felipee@gmail.com>
2025-09-19 21:25:48 -0300

    criando a pagina de destaque pela url do site

--- react-video/src/main.jsx
+++ react-video/src/main.jsx
@@ -2,14 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import TaskPage from './pages/TaskPage.jsx'
 
 import {createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <App />,
   },
+  {
+    path: "/task",
+    element: <TaskPage/>,
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
